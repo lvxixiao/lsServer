396d3f979c5a24ab2743f1a2e3176680c486c33c
lvxixiao <848807250@qq.com>
2025-02-12 14:54:34 +0800

    config

--- etc/start_login.sh
+++ etc/start_login.sh
@@ -1,3 +1,4 @@
-export ROOT=$(cd `dirname $0`; pwd)
-export SKYNETROOT=$(dirname "$PWD")"/3rd/skynet"
-$SKYNETROOT/skynet $ROOT/etc/login.conf
+export ETCPATH=$(cd `dirname $0`; pwd)
+export ROOT=$(dirname $ETCPATH)
+$ROOT/3rd/skynet/skynet $ETCPATH/login.conf
+# $ROOT/co $ETCPATH/login.conf
